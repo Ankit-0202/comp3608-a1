4770a523d6a49c2794b4659dbcaaf70c268b79eb
akap2114 <akap2114@uni.sydney.edu.au>
2023-04-03 20:10:24 +1000

    Ankit submitted

--- tree.py
+++ tree.py
@@ -19,9 +19,9 @@ class Node:
         
         self.state = state
     
-    def add_child(self, child, column, row):
+    def add_child(self, child, column):
         
-        self.children[row][column] = child
+        self.children[column] = child
         child.parent = self
         
     def UTILITY(state):
@@ -33,9 +33,6 @@ class Node:
 
     def EVALUATION(self, state):
         return self.SCORE(state, 'r') - self.SCORE(state, 'y')
-        
-    def NUM_IN_A_ROW(self, count, state, player):
-        return
 
     def SCORE(self, state, player):
         val = self.count_tokens(state, player) + 10 * self.NUM_IN_A_ROW(2, state, player) + 100 * self.NUM_IN_A_ROW(3, state, player) + 1000 * self.NUM_IN_A_ROW(4, state, player)
@@ -47,4 +44,66 @@ class Node:
             for element in row:
                 if element == item:
                     count += 1
+        return count
+    
+    
+    def NUM_IN_A_ROW(self, arr, n, char):
+
+        count = 0
+            
+        # check rows
+        for row in arr:
+            consecutive_count = 0
+            for i in range(len(row)):
+                if row[i] == char:
+                    consecutive_count += 1
+                    if consecutive_count == n:
+                        count += 1
+                        consecutive_count = 0
+                else:
+                    consecutive_count = 0
+        
+        # check columns
+        for j in range(len(arr[0])):
+            consecutive_count = 0
+            for i in range(len(arr)):
+                if arr[i][j] == char:
+                    consecutive_count += 1
+                    if consecutive_count == n:
+                        count += 1
+                        consecutive_count = 0
+                else:
+                    consecutive_count = 0
+        
+        # check diagonals
+        for i in range(len(arr)):
+            for j in range(len(arr[0])):
+                if i + n <= len(arr) and j + n <= len(arr[0]):
+                    # check diagonal from top-left to bottom-right
+                    consecutive_count = 0
+                    for k in range(n):
+                        if arr[i+k][j+k] == char:
+                            consecutive_count += 1
+                            if consecutive_count == n:
+                                count += 1
+                                consecutive_count = 0
+                        else:
+                            consecutive_count = 0
+                            
+                    # check diagonal from bottom-left to top-right
+                    consecutive_count = 0
+                    for k in range(n):
+                        if arr[i+n-1-k][j+k] == char:
+                            consecutive_count += 1
+                            if consecutive_count == n:
+                                count += 1
+                                consecutive_count = 0
+        
+        if count != 0:
+            
+            if n == 2:
+                count = count - self.NUM_IN_A_ROW(self, arr, 4, char)*3 - self.NUM_IN_A_ROW(self, arr, 3, char)*2
+            if n == 3:
+                count = count - self.NUM_IN_A_ROW(self, arr, 4, char)*2
+        
         return count
