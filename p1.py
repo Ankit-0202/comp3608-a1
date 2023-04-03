2c5071dd887ca1f40647780a7dfdb4db4736fa39
akap2114 <akap2114@uni.sydney.edu.au>
2023-04-03 17:11:07 +1000

    Add stuff

--- p1.py
+++ p1.py
@@ -16,31 +16,4 @@ if __name__ == '__main__':
     connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 1)
 
 
-def assign_to_tree(contents):
-    
-
-
-
-
-
-def UTILITY(state):
-    return
-	# if red is winner:
-	# 	return 10000
-	# if yellow is winner
-	# 	return -10000
-
-def EVALUATION(state):
-    return
-	# return SCORE(state, red player) – SCORE(state, yellow player)
-
-def SCORE(state, player):
-    return
-	# return number of tokens of player’s colour +
-	# 	10 * NUM_IN_A_ROW(2, state, player) +
-	# 	100 * NUM_IN_A_ROW(3, state, player) +
-	# 	1000 * NUM_IN_A_ROW(4 or more, state, player)
-
-def NUM_IN_A_ROW(count, state, player):
-    return
-	# returns the number of times that <state> contains a <count>-in-a-row for the given <player>
+# def assign_to_tree(contents):
--- tree.py
+++ tree.py
@@ -1,19 +1,50 @@
 class Node:
     
     gen_number = 0
+    player = 0
     state = 0
+    score = 0
     children =[[0, 0, 0],
                [0, 0, 0],
                [0, 0, 0]]
     parent = 0
     
-    def __init__(self):
-        self.parent = 0
-        self.left_child = 0
-        self.right_child
+    def __init__(self, player, state):
+        self.player = player
+        self.state = state
         
     
     def add_child(self, child, column, row):
         
         self.children[row][column] = child
         child.parent = self
+        
+    def UTILITY(state):
+        return
+        # if red is winner:
+        # 	return 10000
+        # if yellow is winner
+        # 	return -10000
+
+    def EVALUATION(state):
+        return
+        # return SCORE(state, red player) – SCORE(state, yellow player)
+
+    def SCORE(state, player):
+        val = count_tokens(state, player) +
+            10 * NUM_IN_A_ROW(2, state, player) +
+            100 * NUM_IN_A_ROW(3, state, player) +
+            1000 * NUM_IN_A_ROW(4 or more, state, player)
+        return val
+
+    def NUM_IN_A_ROW(count, state, player):
+        return
+        # returns the number of times that <state> contains a <count>-in-a-row for the given <player>
+        
+    def count_tokens(state, item):
+        count = 0
+        for row in state:
+            for element in row:
+                if element == item:
+                    count += 1
+        return count
