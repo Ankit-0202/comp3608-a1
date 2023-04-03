349a89311f90f88a4f999ba81a74f3eb3352a394
akap2114 <akap2114@uni.sydney.edu.au>
2023-04-03 15:07:58 +1000

    Ankit submitted

--- p1.py
+++ p1.py
@@ -16,19 +16,23 @@
 
 
 def UTILITY(state):
+    return
 	# if red is winner:
 	# 	return 10000
 	# if yellow is winner
 	# 	return -10000
 
 def EVALUATION(state):
+    return
 	# return SCORE(state, red player) – SCORE(state, yellow player)
 
 def SCORE(state, player):
+    return
 	# return number of tokens of player’s colour +
 	# 	10 * NUM_IN_A_ROW(2, state, player) +
 	# 	100 * NUM_IN_A_ROW(3, state, player) +
 	# 	1000 * NUM_IN_A_ROW(4 or more, state, player)
 
 def NUM_IN_A_ROW(count, state, player):
+    return
 	# returns the number of times that <state> contains a <count>-in-a-row for the given <player>
