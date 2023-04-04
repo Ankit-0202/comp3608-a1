0b2b9766ff53416d2ea2bc802d9208c42a5a2e25
rosenkor <102002026+rosenkor@users.noreply.github.com>
2023-04-04 17:38:26 +1000

    Hi

--- true_mn.py
+++ true_mn.py
@@ -12,17 +12,18 @@ def true_mn(node: Node, nodes_examined, depth, maximizing = True):
    game_ended = node.UTILITY(node.state)
    if game_ended > 0:
         return game_ended, nodes_examined
-
    if maximizing == True:
         value = -float('inf')
         for child in node.children:
              valueA, nodes_examined = true_mn(child, nodes_examined, depth - 1, False)
+             print(valueA)
              node.score = max(node.score,valueA)
         return value, nodes_examined
    if maximizing == False:
         value = float('inf')
         for child in node.children:
              ValueA, nodes_examined = true_mn(child,nodes_examined, depth - 1, True)
+             print(ValueA)
              node.score = min(node.score, ValueA)
         return value, nodes_examined 
 
