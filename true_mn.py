5404d6d5e58d77189aac5854cdd6d73ca96b6d8c
rosenkor <102002026+rosenkor@users.noreply.github.com>
2023-04-04 22:33:55 +1000

    Update true_mn.py

--- true_mn.py
+++ true_mn.py
@@ -1,21 +1,7 @@
 from tree1 import Node
 
-lock_and_key = [0, 0]
 
 def true_mn(node: Node, nodes_examined, depth, maximizing = True):
-     if depth == 0:
-          game_ended = node.UTILITY(node.state)
-          if game_ended != 0:
-               
-               global lock_and_key
-               
-               if (lock_and_key[1] == 0):
-                    lock_and_key = [get_best_column(node, node.score), 1]
-               
-               return get_best_column(node, node.score), node.UTILITY(node.state), nodes_examined
-          
-          else:
-               return "a", node.EVALUATION(node.state),nodes_examined
    
      game_ended = node.UTILITY(node.state)
      if game_ended != 0:
@@ -23,10 +9,10 @@ def true_mn(node: Node, nodes_examined, depth, maximizing = True):
      if maximizing == True:
           value = -float('inf')
           for child in node.children:
-               column,valueA, nodes_examined = true_mn(child, nodes_examined + 1, depth - 1, False)
+               valueA, nodes_examined = true_mn(child, nodes_examined + 1, depth - 1, False)
                child.score = valueA
                value = max(value,valueA)
-          return column, value, nodes_examined
+          return value, nodes_examined
      
      if maximizing == False:
           value = float('inf')
@@ -34,7 +20,7 @@ def true_mn(node: Node, nodes_examined, depth, maximizing = True):
                column, valueA, nodes_examined = true_mn(child,nodes_examined + 1, depth - 1, True)
                child.score = valueA
                value = min(value, valueA)
-          return column, value, nodes_examined 
+          return value, nodes_examined 
 
 
 def true_ab_pruning(node: Node, depth, alpha, beta):
@@ -47,27 +33,3 @@ def true_ab_pruning(node: Node, depth, alpha, beta):
      return alpha
 
 
-def get_best_column(node: Node, value):
-     # print("Value:", value)
-     scores = [0 for _ in range(7)]
-     count = 0
-     for child in node.children:
-          if child != 0:
-               scores[count] = child.score
-               # print(scores[count])
-               count += 1
-          # print("Scores:", scores)        
-     # print("Index:", scores.index(value))     
-               
-     return scores.index(value)
-               
-     
-     # if maximizing == False:
-     #      return scores.index(value)
-
-     # if maximizing == True:
-     #      return scores.index(value)
-     
-def get_lak():
-     global lock_and_key
-     return lock_and_key
