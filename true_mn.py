53b431e6875e0147e0393f45f1b332d4663dd9bc
rosenkor <102002026+rosenkor@users.noreply.github.com>
2023-04-05 10:36:35 +1000

    New

--- true_mn.py
+++ true_mn.py
@@ -14,8 +14,7 @@ def true_mn(node: Node, nodes_examined, depth, maximizing = True):
                     lock_and_key = [get_best_column(node, node.score), 1]
                
                return get_best_column(node, node.score), node.UTILITY(node.state), nodes_examined
-          else:
-               return -1, node.EVALUATION(node.state),nodes_examined
+          return -1, node.EVALUATION(node.state),nodes_examined
    
      game_ended = node.UTILITY(node.state)
      if game_ended != 0:
@@ -37,15 +36,43 @@ def true_mn(node: Node, nodes_examined, depth, maximizing = True):
           return column, value, nodes_examined 
 
 
-def true_ab_pruning(node: Node, depth, alpha, beta):
+def true_ab_pruning(node: Node, nodes_examined, depth, alpha, beta, maximizing = True):
      nodes_examined = nodes_examined + 1
      if depth == 0:
-          return node.EVALUATION(node.state)
-     for child in node.children:
-          alpha = max(alpha, -true_ab_pruning(child, depth - 1, -beta, -alpha))
-          if beta <= alpha:
-               break
-     return alpha
+               game_ended = node.UTILITY(node.state)
+               if game_ended != 0:
+                    
+                    global lock_and_key
+                    
+                    if (lock_and_key[1] == 0):
+                         lock_and_key = [get_best_column(node, node.score), 1]
+                    
+                    return get_best_column(node, node.score), node.UTILITY(node.state), nodes_examined
+               else:
+                    return "a", node.EVALUATION(node.state),nodes_examined
+     game_ended = node.UTILITY(node.state)
+     if game_ended != 0:
+          return 0, game_ended, nodes_examined
+     if maximizing == True:
+          value = -float('inf')
+          for child in node.children:
+               column,valueA, nodes_examined = true_ab_pruning(child, nodes_examined, depth - 1, alpha, beta, False)
+               child.score = valueA
+               alpha = max(max(value,valueA), alpha)
+               if beta <= alpha:
+                    break
+          return column, value, nodes_examined
+     
+     if maximizing == False:
+          value = float('inf')
+          for child in node.children:
+               column, valueA, nodes_examined = true_ab_pruning(child,nodes_examined, depth - 1, alpha, beta, True)
+               child.score = valueA
+               beta = min(min(value,valueA), beta)
+               if beta <= alpha:
+                    break
+          return column, value, nodes_examined 
+
 
 
 def get_best_column(node: Node, value):
@@ -57,7 +84,7 @@ def get_best_column(node: Node, value):
                scores[count] = child.score
                # print(scores[count])
                count += 1
-          # print("Scores:", scores)        
+     print("Scores:", scores)        
      # print("Index:", scores.index(value))     
                
      return scores.index(value)
