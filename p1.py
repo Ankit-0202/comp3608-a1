8efbc6602a6df1746b54ef3d821ed4a78414a2df
akap2114 <akap2114@uni.sydney.edu.au>
2023-04-05 16:50:08 +1000

    Ankit pushed

--- p1.py
+++ p1.py
@@ -47,36 +47,6 @@ def get_valid_moves(board):
                 break
     return valid_moves
 
-def connect_four_ab(contents, turn, max_depth):
-    
-    new_state = input_to_string(contents)
-    
-    if turn == 'yellow':
-        turn = 'y'
-    if turn == 'red':
-        turn = 'r'
-
-    head_node = Node(turn, new_state)
-
-    create_tree(head_node, turn, max_depth)
-
-    # print(head_node.children[0].player)
-    
-    alpha = -float('inf')
-    beta = float('inf')
-    column, values, nodes_examined = true_ab_pruning(head_node, 0, max_depth, alpha, beta)
-    
-    columnr = get_lak()
-    
-    column = columnr[0]
-
-    
-    # print("hi")
-    # print(f'{column}\n{nodes_examined}')
-    # print("bye")
-
-    return f'{column}\n{nodes_examined}'
-
 
 def connect_four_mm(contents, turn, max_depth):
     
