9b166cb64bbeb1b4e44828bd7b82e5731240f700
akap2114 <akap2114@uni.sydney.edu.au>
2023-04-07 21:14:29 +1000

    f

--- mm/call_minimax.py
+++ mm/call_minimax.py
@@ -12,7 +12,7 @@ def input_to_string(str):
 def connect_four_mm(contents, turn, max_depth):
 
     state = input_to_string(contents)
-    print(state)
+    # print(state)
 
     
     global combs
@@ -38,38 +38,5 @@ def connect_four_mm(contents, turn, max_depth):
     return f'{values}\n{nodes_examined}'
 
 
-
-
 if __name__ == '__main__':
-    # Example function call below, you can add your own to test the connect_four_mm function
-    print(connect_four_mm("ryryrry,rrryyyr,yryyyry,yyyryyy,rryyrrr,.rr..r.", "red", 5))
-    #print(connect_four_mm("ryryryr,yryryry,ryryryr,.......,.......,.......", "yellow", 4))
-    #print()
-    #print(connect_four_mm("ryryr..,rrryy..,yryy...,.yyr...,.ry....,..r....", "red", 10))
-    #print()
-    #print(connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 5))
-    #print()
-    # print("7")
-    #print(connect_four_mm("..y.r..,..y.r..,.......,.......,.......,.......", "red", 1))
-    #print()
-    #print("8")
-    #print(connect_four_mm("..y.r..,..y.r..,.......,.......,.......,.......", "yellow", 1))
-    #print()
-    # print("9")
-    #print(connect_four_mm("..y.r..,..y.r..,..y.r..,.......,.......,.......", "red", 1))
-    #print()
-    # print("10")
-    #print(connect_four_mm("..y.r..,..y.r..,..y.r..,.......,.......,.......", "red", 2))
-    #print()
-    # print("11")
-    #print(connect_four_mm("..y.r..,..y.r..,..y.r..,.......,.......,.......", "red", 3))
-    #print()
-    # print("12")
-    #print(connect_four_mm("r..y..r,r..y..r,......r,.......,.......,.......", "red", 1))
-    #print()
-    # print("13")
-    #print(connect_four_mm("r..y..r,r..y..r,......r,.......,.......,.......", "red", 2))
-    #print()
-    # print("14")
-    #print(connect_four_mm("r..y..r,r..y..r,......r,.......,.......,.......", "red", 3))
-    # print("Done!")
+    print(connect_four_mm("yrryyry,rryrrry, yyryyry,rrryyyr,yyryrry, rryryy.", "red", 4))
