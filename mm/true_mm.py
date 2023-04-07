11d2cfdfa8f54278ff986ce28eee7286ea0311d6
rosenkor <102002026+rosenkor@users.noreply.github.com>
2023-04-07 21:22:15 +1000

    ho

---
+++ mm/true_mm.py
@@ -0,0 +1,45 @@
+from helper_functions import EVALUATION, UTILITY, check_full, simulate_move
+
+def switch_player(player):
+     if player == 'r':
+          player = 'y'
+     else:
+          player = 'r'
+     return player
+
+
+def true_mn(player, original_player, state, nodes_examined, depth, max_depth, maximizing = True):
+     nodes_examined = nodes_examined + 1
+     if UTILITY(state):
+          return UTILITY(state), nodes_examined
+
+     if depth == 0:
+          return EVALUATION(state, original_player),nodes_examined
+     
+     if maximizing == True:
+          value = -float('inf')
+     else:
+          value = float('inf')
+
+     values_array = [None] * 7
+
+     for c in range(7):
+          for r in (range(6)):
+               if state[r][c] == '.':
+                    if not check_full(state):
+                         new_state = simulate_move(state, r, c, player)
+                         if maximizing == True:
+                              valueA, nodes_examined = true_mn(switch_player(player), original_player, new_state, nodes_examined, depth - 1, max_depth, False)
+                         else:
+                              valueA, nodes_examined = true_mn(switch_player(player), original_player, new_state ,nodes_examined, depth - 1, max_depth, True)
+                         values_array[c] = valueA
+                         if maximizing == True:
+                              value = max(value, valueA) 
+                         else:
+                              value = min(value, valueA)
+                    break
+
+     if depth == max_depth:
+          return values_array.index(value), nodes_examined
+     
+     return value, nodes_examined
