c0c05e132e42f89c3afde6121a9daf37725dfb64
akap2114 <akap2114@uni.sydney.edu.au>
2023-04-07 12:34:17 +1000

    Ankit pushed

--- call_negamax.py
+++ call_negamax.py
@@ -1,9 +1,9 @@
-import random
+# import random
 from negamax import negamax
 
 """
 Upon further research, we have decided to use the negamax function, which is a more 
-efficent use of the minimax function with alpha-beta pruning, as negamax is meant for zero-sum games which fit
+efficient use of the minimax function with alpha-beta pruning, as negamax is meant for zero-sum games which fit
 our game of connect four. We have also decided to use the negamax function because it is more efficient. 
 We will also decide to use a random max_depth between 1 and 4 for this case, as it is not being supplied to us. 
 """
@@ -25,7 +25,8 @@ def connect_four_negamax(contents, turn):
     alpha = -float('inf')
     beta = float('inf')
 
-    max_depth = random.randint(1, 4)
+    # max_depth = random.randint(1, 4)
+    max_depth = 2
 
     values, nodes_examined = negamax(turn, turn, state, 0, max_depth, max_depth, alpha, beta)
 
