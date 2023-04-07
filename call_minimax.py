f93b13bf9a2b641e4c6d039c6850cd351d770fef
akap2114 <akap2114@uni.sydney.edu.au>
2023-04-07 11:59:40 +1000

    Ankit pushed

--- call_minimax.py
+++ call_minimax.py
@@ -5,25 +5,19 @@ from true_mn import *
 
 combs = {}
 
-# def load_dict():
+def load_dict():
     
-#     try:
-#         with open('dict_pickle.pickle', 'rb') as handle:
-#             combs = pickle.load(handle)
-#         return combs
-#     except:
-#         return {}
+    try:
+        with open('dict_pickle.pickle', 'rb') as handle:
+            combs = pickle.load(handle)
+        return combs
+    except:
+        return {}
 
-# def write_dict(dict):
-#     with open('dict_pickle.pickle', 'wb') as handle:
-#         pickle.dump(dict, handle, protocol = pickle.HIGHEST_PROTOCOL)
+def write_dict(dict):
+    with open('dict_pickle.pickle', 'wb') as handle:
+        pickle.dump(dict, handle, protocol = pickle.HIGHEST_PROTOCOL)
 
-
-# def dict_to_file(combs):
-#     with open('dict.txt', 'w') as file:
-#         file.write(json.dumps(combs)) # use `json.loads` to do the reverse
-        
-#     file.close()
 
 def input_to_string(str):
     list = str.split(",")
@@ -35,7 +29,7 @@ def connect_four_mm(contents, turn, max_depth):
     state = input_to_string(contents)
     
     global combs
-    # combs = load_dict()
+    combs = load_dict()
     set_dict(combs)
     
     
@@ -54,12 +48,37 @@ def connect_four_mm(contents, turn, max_depth):
     # print(f'{column}\n{nodes_examined}')
     # print("bye")
     
-    # write_dict(combs)
+    write_dict(combs)
 
     return f'{values}\n{nodes_examined}'
 
 
 if __name__ == '__main__':
     # Example function call below, you can add your own to test the connect_four_mm function
-    print(connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 5))
+    # print(connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 5))
+    # print("2")
+    # print(connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 1))
+    # print("3")
+    # print(connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 2))
+    # print("4")
+    # print(connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 3))
+    # print("5")
+    # print(connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 4))
+    # print("7")
+    # print(connect_four_mm("..y.r..,..y.r..,.......,.......,.......,.......", "red", 1))
+    # print("8")
+    # print(connect_four_mm("..y.r..,..y.r..,.......,.......,.......,.......", "yellow", 1))
+    # print("9")
+    # print(connect_four_mm("..y.r..,..y.r..,..y.r..,.......,.......,.......", "red", 1))
+    # print("10")
+    # print(connect_four_mm("..y.r..,..y.r..,..y.r..,.......,.......,.......", "red", 2))
+    # print("11")
+    # print(connect_four_mm("..y.r..,..y.r..,..y.r..,.......,.......,.......", "red", 3))
+    # print("12")
+    # print(connect_four_mm("r..y..r,r..y..r,......r,.......,.......,.......", "red", 1))
+    # print("13")
+    # print(connect_four_mm("r..y..r,r..y..r,......r,.......,.......,.......", "red", 2))
+    # print("14")
+    # print(connect_four_mm("r..y..r,r..y..r,......r,.......,.......,.......", "red", 3))
+    print("Done!")
     # print(combs)
