c4ecee16414c5e32a147a7df2d71255c98dc5ec7
rosenkor <102002026+rosenkor@users.noreply.github.com>
2023-04-19 14:46:37 +1000

    hi

--- dt/program.py
+++ dt/program.py
@@ -4,81 +4,98 @@ import math
 
 
 def classify_dt(training_filename, testing_filename):
-    print("Hi")
     training = pd.read_csv(training_filename, header=None)
     testing = pd.read_csv(testing_filename, header=None)
-    attributes = list(range(training.shape[1] - 1))
-    train_yes = training[training.iloc[:, -1] == 'yes']
-    train_yes = train_yes.reset_index(drop = True)
-    train_no = training[training.iloc[:, -1] == 'no']
-    train_no = train_no.reset_index(drop = True)
-    yes = train_yes.shape[0]
-    no = train_no.shape[0]
-    default = 'none'
-    if yes >= no:
-      default = 'yes'
-    else:
-      default = 'no'
-    tree = decision_tree(training, attributes, default)
+    num_yes = (training[training.iloc[:, -1] == 'yes']).shape[0] 
+    num_no = (training[training.iloc[:, -1] == 'no']).shape[0]
+    default = 'yes' if num_yes >= num_no  else 'no'
+    tree = decision_tree(training, list(range(training.shape[1] - 1)), default)
     results = decision_tree_test(tree, testing)
     print(results)
+    #print_dt(tree)
     return results
   
 def information_gain_chooser(possible_attributes, df):
-  list_of_best = []
+  best_col = None
+  best_gain = -100000000000
   for attribute in possible_attributes:
-    info_ordered_pair = information_gain(df, attribute)
-    list_of_best.append(info_ordered_pair)
-  max_second_item = max(pair[1] for pair in list_of_best)
-  best_col = next(pair[0] for pair in list_of_best if pair[1] == max_second_item)
+    curr_gain = information_gain(df, attribute)
+    if curr_gain > best_gain:
+      best_gain = curr_gain
+      best_col = attribute
   return best_col
     
     
   
   
 def information_gain(df, col_num):
-  pairs_of_logs = []
   train_yes = df[df.iloc[:, -1] == 'yes']
   train_yes = train_yes.reset_index(drop = True)
   train_no = df[df.iloc[:, -1] == 'no']
   train_no = train_no.reset_index(drop = True)
-  yes_probability = train_yes.shape[0] / df.shape[0]
-  no_probability = train_no.shape[0] / df.shape[0]
-  gain_before_splitting = (- yes_probability * math.log(yes_probability, 2)) -  (no_probability * math.log(no_probability, 2))
+  gain_before_splitting = gain(train_yes, train_no)
+  gain_after_splitting = 0
   #First off, we have to get the distinct values of the variable
   col_name = df.columns[col_num]
   unique_values = df[col_name].unique()
   for value in unique_values:
     subset = df[df[col_name] == value]
-    output = subset.columns[-1]
-    count_whole = subset[output].value_counts()
-    yes_prob = (count_whole['yes']/ subset.shape[0]) if 'yes' in count_whole else 0
-    no_prob = (count_whole['no']/ subset.shape[0]) if 'no' in count_whole else 0
-    if yes_prob > 0 and no_prob > 0:
-      gain_intermediate = (- yes_prob * math.log(yes_prob, 2)) -  (no_prob * math.log(no_prob, 2))
-    else:
-      gain_intermediate = 0
-    pairs_of_logs.append(((count_whole['yes'] if 'yes' in count_whole else 0/ df.shape[0]), gain_intermediate))
-  sum_of_subsets = 0
-  for pair in pairs_of_logs:
-    x = pair[0]
-    y = pair[1]
-    product = x * y
-    sum_of_subsets += product
-  return (col_num, sum_of_subsets)
+    subset_yes = subset[subset.iloc[:, -1] == 'yes']
+    subset_yes = subset_yes.reset_index(drop = True)
+    subset_no = subset[subset.iloc[:, -1] == 'no']
+    #print(subset_no)
+    subset_no = subset_no.reset_index(drop = True)
+    gain_middle_splitting = gain(subset_yes, subset_no)
+    gain_after_splitting += (subset.shape[0] / df.shape[0]) * gain_middle_splitting
+    information_gain = gain_before_splitting - gain_after_splitting
+  return information_gain
+
+def gain(yes_df, no_df):
+  if yes_df.empty and no_df.empty:
+    return 0
+  else:
+    yes_probability = yes_df.shape[0] / (yes_df.shape[0] + no_df.shape[0])
+    no_probability = no_df.shape[0] / (yes_df.shape[0] + no_df.shape[0])
+    return -yes_probability * math.log2(yes_probability) - no_probability * math.log2(no_probability) if yes_probability != 0 and no_probability != 0 else 0
+
+
+
+
   
-    
+  
 def decision_tree_test(tree, df):
     results = []
     for index, row in df.iterrows():
-      node = tree
-      #while node.isLeaf == False:
-        #node = [pair[1] for pair in node.child]
-        #print(node)
-        #node = node.child[row[node.attr]][1]
-        #print(node.child)
-        #print(".")
-      #results.append(node.attr)  
+      #print(index)
+      results.append(decision_tree_test_recursive_helper(tree, row))
+    #print(results)
+    return results
+    
+def decision_tree_test_recursive_helper(node, row):
+      """
+      print(node)
+      tree = node
+      while not tree.isLeaf:
+        print(tree.child)
+        if (tree.attr, int):
+          tree = tree.child[1][(tree.child[row[tree.attr]])]
+        else:
+          tree = tree.child[0][1]
+      return tree.attr
+      """
+
+      if node.isLeaf == True:
+        return node.attr
+      else:
+        for i in node.child:
+            if i[0] == row[node.attr]:
+              return decision_tree_test_recursive_helper(i[1], row)
+        return None
+          #node = [pair[1] for pair in node.child][0]
+          #print(node.attr)
+          #node = node.child[row[node.attr]][1]
+          #print(node.child)
+          #print(".")
 
       
 def decision_tree(examples, attributes, default):
@@ -86,23 +103,27 @@ def decision_tree(examples, attributes, default):
     leaf = node(True, default, [])
     return leaf
   
-  example_yes = examples[examples.iloc[:, -1] == 'yes']
-  example_yes = example_yes.reset_index(drop = True)
-  example_no = examples[examples.iloc[:, -1] == 'no']
-  example_no = example_no.reset_index(drop = True)
-  yes = example_yes.shape[0]
-  no = example_no.shape[0]
-  majority = ""
-  #print(type(attributes))
+  example_yes = (examples[examples.iloc[:, -1] == 'yes']).reset_index(drop = True)
+  example_no = (examples[examples.iloc[:, -1] == 'no']).reset_index(drop = True)
+  num_yes = example_yes.shape[0]
+  num_no = example_no.shape[0]
+  majority = default = 'yes' if num_yes >= num_no  else 'no'
 
-  if yes >= no:
-    majority = "yes"
-  else:
-    majority = "no"
-  if yes == 0 or no == 0:
-    same_class = node(True, "yes" if no == 0 else "no", [])
+  new_examples = examples.copy()
+  num_rows = new_examples.shape[0]
+
+  using_row = new_examples.iloc[0, :-1]
+  same_attributes = all(using_row.equals(r[:-1]) for i, r in new_examples.iterrows())
+  print(same_attributes)
+
+  if num_yes == 0 or num_no == 0:
+    #print("hello")
+    same_class = node(True, "yes" if num_no == 0 else "no", [])
     return same_class
   elif not bool(attributes):
+    leaf = node(True, majority, [])
+    return leaf
+  elif same_attributes:
     leaf = node(True, majority, [])
     return leaf
   else:
@@ -111,15 +132,21 @@ def decision_tree(examples, attributes, default):
     col_name = examples.columns[best]
     unique_values = examples[col_name].unique()
     for v_i in unique_values:
-      print(v_i)
       examples_i = examples[examples[col_name] == v_i]
       new_attr = [attributes[i] for i in range(len(attributes)) if attributes[i] != best]
       subtree = decision_tree(examples_i, new_attr, majority)
-      tree.child.append((tree, v_i, node))
-      print(tree.child)
+      tree.child.append((v_i, subtree))
+      #print(tree.child)
   return tree
-    
 
 
 classify_dt('train.csv', 'test.csv')
  
+def print_dt(node, level = 0):
+  if node.isLeaf:
+    print("  " * level, node.attr)
+  else:
+    print("  " * level, node.attr)
+    for child in node.child:
+      print_dt(child[1], level + 1)
+
--- nb/program.py
+++ nb/program.py
@@ -1,38 +1,31 @@
 import pandas as pd
 import statistics as stat
 import math
+import csv
 
 def classify_nb(training_filename, testing_filename):
+    with open(training_filename, 'r') as file:
+      csvreader = csv.reader(file)
+      for row in csvreader:
+        print(row)
     training = pd.read_csv(training_filename, header=None)
     testing = pd.read_csv(testing_filename, header=None)
-    yes_values = []
-    no_values = []
-    results = []
-    train_yes = training[training.iloc[:, -1] == 'yes']
-    train_yes = train_yes.reset_index(drop = True)
-    #print(train_yes)
-    train_no = training[training.iloc[:, -1] == 'no']
-    train_no = train_no.reset_index(drop = True)
-    #print(train_no.shape[0])
+    
+    train_yes = (training[training.iloc[:, -1] == 'yes']).reset_index(drop = True)
+    train_no = (training[training.iloc[:, -1] == 'no']).reset_index(drop = True)
+
     yes_probability = train_yes.shape[0] / training.shape[0]
     no_probability = train_no.shape[0] / training.shape[0]
     yes_means, yes_stddevs = math_tools(train_yes)
     no_means, no_stddevs = math_tools(train_no)
     results = test_nb_classifier(testing, yes_means, yes_stddevs, no_means, no_stddevs, yes_probability, no_probability)
-    #print(results)
-    return results
-  
-
+    training.to_csv('train.csv', index = False)
+    return training.values.tolist()
+ 
   
 def math_tools(df):
-  means = []
-  stddevs = []
-  for column in df.columns[:-1]:
-      stddev = df[column].std()
-      #print(stddev)
-      mean = df[column].mean()
-      means.append(mean)
-      stddevs.append(stddev)
+  stddevs = [stat.stdev(df[column]) for column in df.columns[:-1]]
+  means = [stat.mean(df[column]) for column in df.columns[:-1]]
   return means, stddevs
         
 def test_nb_classifier(testing, yes_means, yes_stddevs, no_means, no_stddevs, yes_probability, no_probability):
@@ -40,81 +33,18 @@ def test_nb_classifier(testing, yes_means, yes_stddevs, no_means, no_stddevs, ye
   for index, row in testing.iterrows():
     yes_final = yes_probability
     no_final = no_probability
-    yes_test_values = []
-    no_test_values = []
-    for column in range(testing.shape[1] - 1):
-      yes_value = probability_density_function(yes_means[column], yes_stddevs[column], row[column])
-      yes_final = yes_final * yes_value
-      no_value = probability_density_function(no_means[column], no_stddevs[column], row[column])
-      no_final = no_final * no_value
-    #print(yes_final, no_final)
-    if yes_final >= no_final:
-      #print('yes')
-      results.append('yes')
-    else:
-      #print('no')
-      results.append('no')
+    for i in range(len(row) - 1):
+        yes_final *= probability_density_function(yes_means[i], yes_stddevs[i], row[i])
+        no_final *= probability_density_function(no_means[i], no_stddevs[i], row[i])
+    results.append('no') if yes_final < no_final else results.append('yes')
   return results
-      
       
   
   
 def probability_density_function(mean, stddev, value):
-  coefficent = (1 / (stddev * math.sqrt(2 * math.pi)))
-  exponent =  math.exp(((-math.pow((value - mean), 2))) / (2 * math.pow(stddev, 2)))
-  #print(coefficent)
-  prob_den = (coefficent * exponent)
+  prob_den = (1 / (stddev * math.sqrt(2 * math.pi))) * math.exp(-((value - mean)**2 / (2 * stddev**2)))  
   return prob_den
 
 
-  
-    
 
-data = """0.352941,0.670968,0.489796,0.304348,0.169471,0.314928,0.234415,0.483333,yes
-0.058824,0.264516,0.428571,0.23913,0.169471,0.171779,0.116567,0.166667,no
-0.470588,0.896774,0.408163,0.23913,0.169471,0.104294,0.253629,0.183333,yes
-0.058824,0.290323,0.428571,0.173913,0.096154,0.202454,0.038002,0,no
-0,0.6,0.163265,0.304348,0.185096,0.509202,0.943638,0.2,yes
-0.294118,0.464516,0.510204,0.23913,0.169471,0.151329,0.052519,0.15,no
-0.176471,0.219355,0.265306,0.271739,0.088942,0.261759,0.072588,0.083333,yes
-0.588235,0.458065,0.489796,0.23913,0.169471,0.349693,0.023911,0.133333,no
-0.117647,0.987097,0.469388,0.413043,0.635817,0.251534,0.034159,0.533333,yes
-0.470588,0.522581,0.734694,0.23913,0.169471,0.291564,0.065756,0.55,yes
-0.235294,0.425806,0.693878,0.23913,0.169471,0.396728,0.048249,0.15,no
-0.588235,0.8,0.510204,0.23913,0.169471,0.404908,0.195986,0.216667,yes
-0.588235,0.612903,0.571429,0.23913,0.169471,0.182004,0.581981,0.6,no
-0.058824,0.935484,0.367347,0.173913,1,0.243354,0.136635,0.633333,yes
-0.294118,0.787097,0.489796,0.130435,0.19351,0.155419,0.217336,0.5,yes
-0.411765,0.36129,0.489796,0.23913,0.169471,0.241309,0.173356,0.183333,yes
-0,0.477419,0.612245,0.434783,0.259615,0.564417,0.201964,0.166667,yes
-0.411765,0.406452,0.510204,0.23913,0.169471,0.233129,0.075149,0.166667,yes"""
-data2 = pd.DataFrame([x.split(',') for x in data.split('\n')])
-#data2.to_csv('train.csv', index = False)
-#data2 = data2.to_numeric(data2[:-1], downcast = 'float')
-#print(data2[0].values[0])
-dataA = """0.352941,0.670968,0.489796,0.304348,0.169471,0.314928,0.234415,0.483333
-0.058824,0.264516,0.428571,0.23913,0.169471,0.171779,0.116567,0.166667
-0.470588,0.896774,0.408163,0.23913,0.169471,0.104294,0.253629,0.183333
-0.058824,0.290323,0.428571,0.173913,0.096154,0.202454,0.038002,0
-0,0.6,0.163265,0.304348,0.185096,0.509202,0.943638,0.2
-0.294118,0.464516,0.510204,0.23913,0.169471,0.151329,0.052519,0.15
-0.176471,0.219355,0.265306,0.271739,0.088942,0.261759,0.072588,0.083333
-0.588235,0.458065,0.489796,0.23913,0.169471,0.349693,0.023911,0.133333
-0.117647,0.987097,0.469388,0.413043,0.635817,0.251534,0.034159,0.533333
-0.470588,0.522581,0.734694,0.23913,0.169471,0.291564,0.065756,0.55
-0.235294,0.425806,0.693878,0.23913,0.169471,0.396728,0.048249,0.15
-0.588235,0.8,0.510204,0.23913,0.169471,0.404908,0.195986,0.216667
-0.588235,0.612903,0.571429,0.23913,0.169471,0.182004,0.581981,0.6
-0.058824,0.935484,0.367347,0.173913,1,0.243354,0.136635,0.633333
-0.294118,0.787097,0.489796,0.130435,0.19351,0.155419,0.217336,0.5
-0.411765,0.36129,0.489796,0.23913,0.169471,0.241309,0.173356,0.183333
-0,0.477419,0.612245,0.434783,0.259615,0.564417,0.201964,0.166667
-0.411765,0.406452,0.510204,0.23913,0.169471,0.233129,0.075149,0.166667"""
-data2A = pd.DataFrame([x.split(',') for x in dataA.split('\n')])
-#data2A.to_csv('test.csv', index = False)
-
-
-#print(data2[0].values)
-#print(stat.stdev(data2[0].values))
-
-classify_nb('train.csv', 'test.csv')
+print(classify_nb('train.csv', 'test.csv'))
